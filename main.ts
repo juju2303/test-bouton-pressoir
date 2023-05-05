8f14f198db27e227ff1018979e13efe699fb9a1f
juju2303 <132678792+juju2303@users.noreply.github.com>
2023-05-05 16:39:12 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -24,6 +24,13 @@ input.onButtonPressed(Button.A, function () {
     }
     basic.pause(2000)
     basic.showString("presse B")
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
 })
 input.onButtonPressed(Button.B, function () {
     music.playMelody("C5 A B G A F G E ", 120)
@@ -31,3 +38,10 @@ input.onButtonPressed(Button.B, function () {
     music.playMelody("E G F A G B A C5 ", 120)
 })
 basic.showString("presse A")
+basic.showLeds(`
+    . . # . .
+    . # . . .
+    # # # # #
+    . # . . .
+    . . # . .
+    `)
