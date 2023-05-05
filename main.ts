0f174d094db1765b779ce8726c61721f4b18ccd7
juju2303 <132678792+juju2303@users.noreply.github.com>
2023-05-05 16:33:24 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,33 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 2; index++) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    }
+    basic.pause(2000)
+    basic.showString("presse B")
+})
+input.onButtonPressed(Button.B, function () {
+    music.playMelody("C5 A B G A F G E ", 120)
+    basic.pause(2000)
+    music.playMelody("E G F A G B A C5 ", 120)
+})
+basic.showString("presse A")
